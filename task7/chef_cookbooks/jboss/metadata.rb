9b132b4f848b12386e77daad2c13c4c27f58321b
Evgeniy Krupen <evgeniy_krupen@epam.com>
2016-09-15 09:15:03 +0300

    Update metadata.rb

--- task7/chef_cookbooks/jboss/metadata.rb
+++ task7/chef_cookbooks/jboss/metadata.rb
@@ -5,11 +5,4 @@ license 'all_rights'
 description 'Installs/Configures jboss'
 long_description 'Installs/Configures jboss'
 version '0.1.0'
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/jboss/issues' if respond_to?(:issues_url)
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/jboss' if respond_to?(:source_url)
+depends 'java'

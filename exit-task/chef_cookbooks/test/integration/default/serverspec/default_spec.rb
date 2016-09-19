16e7e3b36be039a39c1e20249d5c16eb684fc3c4
Evgeniy Krupen <evgeniy_krupen@epam.com>
2016-09-19 20:39:09 +0300

    Update default_spec.rb

--- exit-task/chef_cookbooks/test/integration/default/serverspec/default_spec.rb
+++ exit-task/chef_cookbooks/test/integration/default/serverspec/default_spec.rb
@@ -7,7 +7,25 @@ describe port('22') do
   it { should be_listening }
 end
 
+# check jenkins running
 describe service('jenkins') do
     it { should be_enabled }
     it { should be_running }
+end
+
+#check oinstalled tools
+describe package('java') do
+  it { should be_installed }
+end
+
+describe package('tomcat') do
+  it { should be_installed }
+end
+
+describe package('nginx') do
+  it { should be_installed }
+end
+
+describe package('jenkins') do
+  it { should be_installed }
 end
